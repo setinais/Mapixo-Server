d1f4b5feaca0118cef3202d5f0936c8c88a66644
Vinnicyus Carvalho <vynny.cg@gmail.com>
2021-01-20 18:58:12 -0300

    app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                        <a href="{{ route('file') }}">Download APP</a>
+                        <a href="{{ route('file') }}" download target="_blank">Download APP</a>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
